fcbad0e476104384353a13ef187822bcce077398
Piyush sharma <piyush67.sharm@gmail.com>
2022-08-25 10:51:11 +0530

    4th commit

--- q3.js
+++ q3.js
@@ -1,13 +1,13 @@
 let data=[{id:4,data:3},{id:2,data:4},{id:3,data:2},{id:3,data:2}]
 
-function filterArray(data){
+function filterArray(dataid){
     let filteredData=[]
 
     for(let i=0;i<data.length;i++){
-        if(data[i].id===3)
+        if(data[i].id===id)
         filteredData.push(data[i])
     }
     return filteredData
 }
 
-console.log(filterArray(data))
+console.log(filterArray(data,4))
